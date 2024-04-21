cf44608a99d264f61f2569499dc9808a17f9b10c
ZachFlores <zjflores@email.sc.edu>
2024-04-21 17:55:05 -0400

    adding final proj

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb+srv://nickflores34:P60Ax9XPHtmofN0x@cluster0.grkm6gm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB Atlas:", error);
+  });
+
+const reviewSchema = new mongoose.Schema({
+  name: String,
+  review: String,
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+app.post("/reviews", async (req, res) => {
+  try {
+    const { name, review } = req.body;
+    const newReview = new Review({ name, review });
+    await newReview.save();
+    res.status(201).json(newReview);
+  } catch (error) {
+    console.error("Error adding review:", error);
+    res.status(500).json({ error: "Failed to add review" });
+  }
+});
+
+app.get("/reviews", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).json({ error: "Failed to fetch reviews" });
+  }
+});
+
+app.delete("/reviews/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Review.findByIdAndDelete(id);
+    res.status(200).json({ message: "Review deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting review:", error);
+    res.status(500).json({ error: "Failed to delete review" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
